3701672df70b22290a59ed8c704a1ac7903909ad
RoeyPrat <roey.prat@gmail.com>
2013-12-05 15:46:38 +0200

    search button

--- Where2Park/src/com/roey/where2park/MainActivity.java
+++ Where2Park/src/com/roey/where2park/MainActivity.java
@@ -2,6 +2,7 @@ package com.roey.where2park;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClickSearch(View view){
+    	onSearchRequested();
     }
 }
