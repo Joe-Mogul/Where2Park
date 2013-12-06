6f02df204a08df6c07a49a0c4ef7bc501e37a073
RoeyPrat <roey.prat@gmail.com>
2013-12-06 12:52:00 +0200

    finally got search to start new activity

--- Where2Park/src/com/roey/where2park/SearchableActivity.java
+++ Where2Park/src/com/roey/where2park/SearchableActivity.java
@@ -1,9 +1,9 @@
 package com.roey.where2park;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -13,7 +13,6 @@ public class SearchableActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_searchable);
-
 	    // Get the intent, verify the action and get the query
 	    Intent intent = getIntent();
 	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
