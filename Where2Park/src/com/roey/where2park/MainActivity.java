a77a9294ac2783b868bd4db02f35bd5bf310c699
RoeyPrat <roey.prat@gmail.com>
2013-12-07 19:01:47 +0200

    location updates

--- Where2Park/src/com/roey/where2park/MainActivity.java
+++ Where2Park/src/com/roey/where2park/MainActivity.java
@@ -1,22 +1,34 @@
 package com.roey.where2park;
 
 import android.app.Activity;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	LocationClient mLocationClient;
+	LocationRequest mLocationRequest;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mLocationClient = new LocationClient(this, this, this);
 
 		GoogleMap mMap;
 		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
@@ -24,17 +36,72 @@ public class MainActivity extends Activity {
 		.position(new LatLng(32.777539, 35.023906))
 		.title("Hello world"));
 
-		PolygonOptions rectOptions = new PolygonOptions()
-		.add(new LatLng(37.35, -122.0),
-				new LatLng(32.777559, 35.023936),
-				new LatLng(32.777559, 35.023906),
-				new LatLng(32.777539, 35.023936),
-				new LatLng(32.777539, 35.023906));
-
-		//Get back the mutable Polygon
-		Polygon polygon = mMap.addPolygon(rectOptions);
 	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mLocationClient.connect();
+		mLocationRequest = LocationRequest.create();
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		mLocationRequest.setInterval(5000);
+		mLocationRequest.setFastestInterval(1000);
+	}
+
+
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+
+
 	public void onClickSearch(View view){
 		onSearchRequested();
+	}
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+	       if (connectionResult.hasResolution()) {
+	            try {
+	                // Start an Activity that tries to resolve the error
+	                connectionResult.startResolutionForResult(
+	                        this,
+	                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+	                /*
+	                 * Thrown if Google Play services canceled the original
+	                 * PendingIntent
+	                 */
+	            } catch (IntentSender.SendIntentException e) {
+	                // Log the error
+	                e.printStackTrace();
+	            }
+	        } else {
+	        	//If no resolution is available, display a dialog to the user with the error.
+	            Log.d("error connecting to location",String.valueOf(connectionResult.getErrorCode()));
+	            }
+
+	}
+	
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    
+	@Override
+	public void onConnected(Bundle dataBundle) {
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+         Location mCurrentLocation = mLocationClient.getLastLocation();
+         float acc = mCurrentLocation.getAccuracy();
+         Toast.makeText(this, "acc = "+ acc , Toast.LENGTH_LONG).show();
+         
+	}
+	@Override
+	public void onDisconnected() {
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
 	}
 }
